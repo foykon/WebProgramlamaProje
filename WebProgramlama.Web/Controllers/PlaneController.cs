32b855734da26bb86523ddd6f3d56bf8ac5944ac
foykon <foykon9@gmail.com>
2023-11-29 01:15:13 +0300

    Layout fixes FURKAN

---
+++ WebProgramlama.Web/Controllers/PlaneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProgramlama.Web.Controllers
+{
+    public class PlaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
